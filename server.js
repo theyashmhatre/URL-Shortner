533fcfde4e5d291f09838199e641b04ebdcf89f1
YashMhatre <yashmhatre62@gmail.com>
2020-08-25 02:20:17 +0530

    minor change

--- server.js
+++ server.js
@@ -49,9 +49,7 @@ app.patch("/:slug", async (req, res, callback) => {
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
 }
 
 
