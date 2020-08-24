be6ec9572d07821bd604a986dd3e35c128a07285
YashMhatre <yashmhatre62@gmail.com>
2020-08-25 02:35:44 +0530

    min

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
