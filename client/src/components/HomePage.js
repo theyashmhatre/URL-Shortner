4ec3f95646c586dcaf1a2fb28165a6288c1b65c9
YashMhatre <yashmhatre62@gmail.com>
2020-08-26 23:35:04 +0530

    new errors

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -7,21 +7,22 @@ function HomePage() {
     const [username, setUsername] = useState();
 
     const history = useHistory();
-    const register = () => history.push("/register");
-    const login = () => history.push("/login");
     const logout = () => setUserData({
         token: undefined,
         user: undefined,
     });
 
+    useEffect(() => {
+        if (!userData.user) { history.push("/login"); setUsername(""); }
+        else { setUsername(userData.user.name); }
+    });
     return (
         <div>
 
             <div>
                 <h1 className="heading-top">Welcome to URL Shortner, {username}</h1>
             </div>
-            {userData.user ? <div className="text-center" ><button onClick={login} className="btn btn-dark" style={{ paddingRight: "10px" }}>Log in</button><button onClick={register} className="btn btn-dark" style={{ paddingRight: "10px" }}>Register</button></div> :
-                <div className="text-center"><button onClick={logout} className="btn btn-dark">Logout</button></div>}
+            <div className="text-center"><button onClick={logout} className="btn btn-dark">Logout</button></div>
         </div>
     );
 }
