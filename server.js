4aaf32cbcac15554f429d85966cccf257c57c62d
YashMhatre <yashmhatre62@gmail.com>
2020-08-26 19:35:33 +0530

    trying to fix error refresh

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.patch("/:slug", async (req, res, callback) => {
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.static(path.join(__dirname, 'client/public')));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
