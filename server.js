cefabc55a72e89b19281da1a2ac54a40de15787a
YashMhatre <yashmhatre62@gmail.com>
2020-08-26 21:38:59 +0530

    new errors

--- server.js
+++ server.js
@@ -50,9 +50,11 @@ app.patch("/:slug", async (req, res, callback) => {
 
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/public')));
+
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
 });
 
 
