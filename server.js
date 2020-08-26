9328040d2375549da7cef4aef6bfdc27bae2568b
YashMhatre <yashmhatre62@gmail.com>
2020-08-26 21:25:06 +0530

    new errors

--- server.js
+++ server.js
@@ -49,12 +49,13 @@ app.patch("/:slug", async (req, res, callback) => {
 });
 
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(express.static(path.join(__dirname, 'client/public')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/public')));
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
+
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
